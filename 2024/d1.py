392ea4d93e5e95ab57e8bab8c58a6c37f3acf717
Eliott Flechtner <eliott.flechtner@gmail.com>
2024-12-01 20:22:40 +0900

    [2024][d1][feat]: initial solutions submit

---
+++ 2024/d1.py
@@ -0,0 +1,15 @@
+def find_total_distance(l1,l2):
+    return sum([abs(l1[i]-l2[i]) for i in range(len(l1))])
+
+def compute_similarity_score(l1,l2):
+    return sum([n*l2.count(n) for n in l1])
+
+if __name__ == "__main__":
+    with open("2024/input.txt") as f:
+        l1,l2 = [],[]
+        for line in f.readlines():
+            l,r = (e.strip() for e in line.split(" ") if e != '')
+            l1.append(int(l))
+            l2.append(int(r))
+        # print(find_total_distance(sorted(l1),sorted(l2)))
+        print(compute_similarity_score(l1,l2))
